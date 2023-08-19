f3a19b82812f6c7b810f2049bb05d4d789f829ec
Felipe Rocha <felipemota.rocha@gmail.com>
2023-08-19 13:05:18 -0300

    feat: add GetUserByIdUseCase

---
+++ src/use-cases/get-user-by-id.js
@@ -0,0 +1,11 @@
+import { PostgresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id.js'
+
+export class GetUserByIdUseCase {
+    async execute(userId) {
+        const getUserByIdRepository = new PostgresGetUserByIdRepository()
+
+        const user = await getUserByIdRepository.execute(userId)
+
+        return user
+    }
+}
