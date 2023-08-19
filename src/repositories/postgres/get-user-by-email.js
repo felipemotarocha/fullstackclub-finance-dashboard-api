7a364f0e5d1c06a764fa31e5c9a1e2cbf91f85f2
Felipe Rocha <felipemota.rocha@gmail.com>
2023-08-19 13:22:43 -0300

    feat: add GetUserByEmailRepository

---
+++ src/repositories/postgres/get-user-by-email.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresGetUserByEmailRepository {
+    async execute(email) {
+        const user = await PostgresHelper.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email],
+        )
+
+        return user[0]
+    }
+}
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,10 +2,22 @@ import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
         // TODO: verificar se o e-mail já está em uso
+        const postgresGetUserByEmailRepository =
+            new PostgresGetUserByEmailRepository()
+
+        const userWithProvidedEmail =
+            await postgresGetUserByEmailRepository.execute(
+                createUserParams.email,
+            )
+
+        if (userWithProvidedEmail) {
+            throw new Error('The provided e-mail is already in use.')
+        }
 
         // gerar ID do usuário
         const userId = uuidv4()
