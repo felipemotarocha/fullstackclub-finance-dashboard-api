d5a45e72dc03167080c41740386e941401daa403
Felipe Rocha <felipemota.rocha@gmail.com>
2023-08-19 13:03:45 -0300

    feat: add PostgresGetUserByIdRepository

---
+++ src/repositories/postgres/get-user-by-id.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresGetUserByIdRepository {
+    async execute(userId) {
+        const user = await PostgresHelper.query(
+            'SELECT * FROM users WHERE id = $1',
+            [userId],
+        )
+
+        return user[0]
+    }
+}
