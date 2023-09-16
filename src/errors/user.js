b6cfe4bcc046f487e65e92a85dddd11e1df64bbb
Felipe Rocha <felipemota.rocha@gmail.com>
2023-09-16 16:02:36 -0300

    feat: add CreateTransactionUseCase

--- src/errors/user.js
+++ src/errors/user.js
@@ -4,3 +4,10 @@ export class EmailAlreadyInUseError extends Error {
         this.name = 'EmailAlreadyInUseError'
     }
 }
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`User with id ${userId} not found.`)
+        this.name = 'UserNotFoundError'
+    }
+}
---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
+    async execute(createTransactionParams) {
+        const userId = createTransactionParams.userId
+
+        const user = await this.getUserByIdRepository.execute(userId)
+
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+
+        const transactionId = uuidv4()
+
+        const transaction = await this.createTransactionRepository.execute({
+            ...createTransactionParams,
+            id: transactionId,
+        })
+
+        return transaction
+    }
+}
