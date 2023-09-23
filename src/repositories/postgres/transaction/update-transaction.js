6664d3ce4eab1276380de4f3c94ff35423089616
Felipe Rocha <felipemota.rocha@gmail.com>
2023-09-23 14:45:09 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,29 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE transactions
+            SET ${updateFields.join(', ')} 
+            WHERE id = $${updateValues.length}
+            RETURNING *
+        `
+
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updatedUser[0]
+    }
+}
