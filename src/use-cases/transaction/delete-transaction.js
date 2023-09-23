72f1744da9fea6a3c1a89643564250fad3b7683f
Felipe Rocha <felipemota.rocha@gmail.com>
2023-09-23 15:35:24 -0300

    feat: add DeleteTransactionUseCase

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,13 @@
+export class DeleteTransactionUseCase {
+    constructor(deleteTransactionRepository) {
+        this.deleteTransactionRepository = deleteTransactionRepository
+    }
+
+    async execute(transactionId) {
+        const transaction = await this.deleteTransactionRepository.execute(
+            transactionId,
+        )
+
+        return transaction
+    }
+}
