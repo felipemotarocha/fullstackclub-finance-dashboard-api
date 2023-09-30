9ad73eb2b0a0d1d0d9e27a94d0ec4f63997b37af
Felipe Rocha <felipemota.rocha@gmail.com>
2023-09-30 16:38:06 -0300

    feat: use get_user_balance function in PostgresGetUserBalanceRepository

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -3,17 +3,7 @@ import { PostgresHelper } from '../../../db/postgres/helper.js'
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
         const balance = await PostgresHelper.query(
-            `SELECT
-                SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-                SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-                SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-                (
-                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-                ) AS balance
-            FROM transactions
-            WHERE user_id = $1;`,
+            `SELECT * from get_user_balance($1)`,
             [userId],
         )
 
