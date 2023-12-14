05e5656844853419020219101fcf1014546f4ae3
Felipe Rocha <felipemota.rocha@gmail.com>
2023-12-14 14:56:56 -0300

    feat: add 'add_user_schema' migration

---
+++ prisma/migrations/20231214175518_add_user_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
