337bddf425129a2cd14207208ad6d27cf99cdf7a
Felipe Rocha <felipemota.rocha@gmail.com>
2023-12-14 16:01:05 -0300

    fix: ensure GetUserBalanceRepository returns totals as string

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client'
 import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
@@ -38,11 +39,13 @@ export class PostgresGetUserBalanceRepository {
             },
         })
 
-        const _totalEarnings = totalEarnings || 0
-        const _totalExpenses = totalExpenses || 0
-        const _totalInvestments = totalInvestments || 0
+        const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
+        const _totalExpenses = totalExpenses || new Prisma.Decimal(0)
+        const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
 
-        const balance = _totalEarnings - _totalExpenses - _totalInvestments
+        const balance = new Prisma.Decimal(
+            _totalEarnings - _totalExpenses - _totalInvestments,
+        )
 
         return {
             earnings: _totalEarnings,
