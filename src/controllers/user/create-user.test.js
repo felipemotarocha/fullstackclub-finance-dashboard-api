a88d5e4ce1bfff9245df4df849464fee55543c44
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-02 16:29:23 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -86,4 +86,24 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not valid', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Felipe',
+                last_name: 'Rocha',
+                email: 'fe',
+                password: '1234567',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
