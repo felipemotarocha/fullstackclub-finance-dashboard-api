a538a3b6ceecd8f93d4ceba20b3eb350765bcd6c
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-06 15:14:38 -0300

    test: ensure DeleteUserController returns 500 if DeleteUserUseCase throws

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -61,4 +61,18 @@ describe('DeleteUserController', () => {
         // assert
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 if DeleteUserUseCase throws', async () => {
+        // arrange
+        const { sut, deleteUserUseCase } = makeSut()
+        jest.spyOn(deleteUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
 })
