ebb0486b7dc2764c18118b196e34075e2d744599
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-16 15:32:00 -0300

    test: ensure CreateTransactionController returns 400 when missing user_id

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -38,4 +38,20 @@ describe('Create Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(201)
     })
+
+    it('should return 400 when missing user_id', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                user_id: undefined,
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
