c3cd0696e418b8959d8fd93008623a2588f5aa02
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-16 16:39:53 -0300

    test: ensure UpdateTransactionController returns 500 when UpdateTransactionUseCase throws

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -108,4 +108,18 @@ describe('Update Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 500 when UpdateTransactionUseCase throws', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const response = await sut.execute(baseHttpRequest)
+
+        // assert
+        expect(response.statusCode).toBe(500)
+    })
 })
