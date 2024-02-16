a1796ee43c2d2021dd28e2c13c22de316c518649
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-16 16:01:30 -0300

    test: ensure GetTransactionsByUserIdController returns 400 when missing user id

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -35,4 +35,17 @@ describe('Get Transaction By User ID Controller', () => {
         // assert
         expect(response.statusCode).toBe(200)
     })
+
+    it('should return 400 when missing userId param', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            query: { userId: undefined },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
