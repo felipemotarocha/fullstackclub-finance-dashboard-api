f718e1e590f524ea23cfef5737df06e7b5f52434
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-16 15:34:34 -0300

    test: ensure CreateTransactionController returns 400 when date is invalid

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -118,4 +118,20 @@ describe('Create Transaction Controller', () => {
         // assert
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when date is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                date: 'invalid_date',
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
 })
