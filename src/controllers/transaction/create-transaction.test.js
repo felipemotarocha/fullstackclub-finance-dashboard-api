47d8218feaeebe63666ed2365d64885f2f88f0ed
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-16 15:37:43 -0300

    test: ensure CreateTransactionController returns 201 when creating investment transaction

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -55,6 +55,22 @@ describe('Create Transaction Controller', () => {
         expect(response.statusCode).toBe(201)
     })
 
+    it('should return 201 when creating transaction successfully (investment)', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: 'INVESTMENT',
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(201)
+    })
+
     it('should return 400 when missing user_id', async () => {
         // arrange
         const { sut } = makeSut()
