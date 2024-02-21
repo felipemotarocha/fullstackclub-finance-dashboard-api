8afaf6cd079680920a59c1e597ad88a9f42325b6
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-21 20:42:08 -0300

    test: ensure GetUserBalanceUseCase throws if GetUserBalanceRepository throws

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -108,4 +108,18 @@ describe('GetUserBalanceUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if GetUserBalanceRepository throws', async () => {
+        // arrange
+        const { sut, getUserBalanceRepository } = makeSut()
+        jest.spyOn(getUserBalanceRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(faker.string.uuid())
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
