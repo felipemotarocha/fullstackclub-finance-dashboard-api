bcc419fc9ad603f9d43387517396501c191d93b6
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-21 19:55:02 -0300

    test: ensure CreateUserUseCase throws if PasswordHasherAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -153,4 +153,18 @@ describe('Create User Use Case', () => {
         // assert
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if PasswordHasherAdapter throws', async () => {
+        // arrange
+        const { sut, passwordHasherAdapter } = makeSut()
+        jest.spyOn(passwordHasherAdapter, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(user)
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
