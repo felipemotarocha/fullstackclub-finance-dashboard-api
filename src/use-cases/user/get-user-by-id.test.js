c3396ca0d1402a558dadb5bf133a2c7735d38adb
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-21 20:50:31 -0300

    test: ensure GetUserByIdUseCase throws if GetUserByIdRepository throws

--- src/use-cases/user/get-user-by-id.test.js
+++ src/use-cases/user/get-user-by-id.test.js
@@ -50,4 +50,18 @@ describe('GetUserByIdUseCase', () => {
         // assert
         expect(executeSpy).toHaveBeenCalledWith(userId)
     })
+
+    it('should throw if GetUserByIdRepository throws', async () => {
+        // arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(faker.string.uuid())
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
