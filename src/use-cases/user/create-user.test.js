04bf31960d629df8b0b536a4643eead2a59696cb
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-21 19:54:29 -0300

    test: ensure CreateUserUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -139,4 +139,18 @@ describe('Create User Use Case', () => {
         // assert
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        // arrange
+        const { sut, idGeneratorAdapter } = makeSut()
+        jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        // act
+        const promise = sut.execute(user)
+
+        // assert
+        await expect(promise).rejects.toThrow()
+    })
 })
