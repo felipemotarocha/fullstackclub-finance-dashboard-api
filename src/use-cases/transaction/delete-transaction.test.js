6c1e3d97222463df913013f87e676c04452aa64a
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-27 11:24:40 -0300

    test: ensure DeleteTransactionUseCase calls DeleteTransactionRepository with correct params

--- src/use-cases/transaction/delete-transaction.test.js
+++ src/use-cases/transaction/delete-transaction.test.js
@@ -45,4 +45,20 @@ describe('DeleteTransactionUseCase', () => {
             id: id,
         })
     })
+
+    it('should call DeleteTransactionRepository with correct params', async () => {
+        // arrange
+        const { sut, deleteTransactionRepository } = makeSut()
+        const deleteTransactionRepositorySpy = jest.spyOn(
+            deleteTransactionRepository,
+            'execute',
+        )
+        const id = faker.string.uuid()
+
+        // act
+        await sut.execute(id)
+
+        // expect
+        expect(deleteTransactionRepositorySpy).toHaveBeenCalledWith(id)
+    })
 })
