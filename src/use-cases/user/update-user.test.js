a32942573dabba4d9ae46f03f6d3dc0070a865e8
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-27 10:20:08 -0300

    test: ensure UpdateUserUseCase calls UpdateUserRepository with correct params

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -117,4 +117,28 @@ describe('UpdateUserUseCase', () => {
             new EmailAlreadyInUseError(user.email),
         )
     })
+
+    it('should call UpdateUserRepository with correct params', async () => {
+        // arrange
+        const { sut, updateUserRepository } = makeSut()
+        const updateUserRepositorySpy = jest.spyOn(
+            updateUserRepository,
+            'execute',
+        )
+        const updateUserParams = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            password: user.password,
+        }
+
+        // act
+        await sut.execute(user.id, updateUserParams)
+
+        // assert
+        expect(updateUserRepositorySpy).toHaveBeenCalledWith(user.id, {
+            ...updateUserParams,
+            password: 'hashed_password',
+        })
+    })
 })
