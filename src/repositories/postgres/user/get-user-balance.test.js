50f1c45f04569caf56a571be762f8d253d5f029e
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-07 19:10:35 -0300

    test: ensure PostgresGetUserBalanceRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -63,4 +63,40 @@ describe('PostgresGetUserBalanceRepository', () => {
         expect(result.investments.toString()).toBe('6000')
         expect(result.balance.toString()).toBe('2000')
     })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserBalanceRepository()
+        const prismaSpy = jest.spyOn(prisma.transaction, 'aggregate')
+
+        await sut.execute(fakeUser.id)
+
+        expect(prismaSpy).toHaveBeenCalledTimes(3)
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+    })
 })
