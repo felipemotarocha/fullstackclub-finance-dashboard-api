e2e6b4788fa0c1fb7e60cb7ca7ba5876c08b4ee5
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-07 19:14:47 -0300

    test: ensure PostgresGetUserByEmailRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-by-email.test.js
+++ src/repositories/postgres/user/get-user-by-email.test.js
@@ -12,4 +12,18 @@ describe('GetUserByEmailRepository', () => {
 
         expect(result).toStrictEqual(user)
     })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByEmailRepository()
+
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(fakeUser.email)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                email: fakeUser.email,
+            },
+        })
+    })
 })
