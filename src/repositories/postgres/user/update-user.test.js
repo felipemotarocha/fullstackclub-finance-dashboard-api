c613aa236ba5ed70eae9f151246f074dd15eb612
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-07 19:28:59 -0300

    test: ensure PostgresUpdateUserRepository throws if Prisma throws

--- src/repositories/postgres/user/update-user.test.js
+++ src/repositories/postgres/user/update-user.test.js
@@ -38,4 +38,13 @@ describe('PostgresUpdateUserRepository', () => {
             data: updateUserParams,
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresUpdateUserRepository()
+        jest.spyOn(prisma.user, 'update').mockRejectedValueOnce(new Error())
+
+        const promise = sut.execute(updateUserParams)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
