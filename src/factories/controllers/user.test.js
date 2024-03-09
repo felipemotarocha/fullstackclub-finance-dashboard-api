8cb6cded3f9ff1cd5161fefe886a4374353dd926
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-09 00:18:11 -0300

    test: ensure makeGetUserBalanceController returns valid GetUserBalanceController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,12 +1,14 @@
 import {
     CreateUserController,
     DeleteUserController,
+    GetUserBalanceController,
     GetUserByIdController,
     UpdateUserController,
 } from '../../controllers'
 import {
     makeCreateUserController,
     makeDeleteUserController,
+    makeGetUserBalanceController,
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './user'
@@ -28,5 +30,11 @@ describe('User Controller Factories', () => {
 
     it('should return a valid DeleteUserController instance', () => {
         expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
+    })
+
+    it('should return a valid GetUserBalanceController instance', () => {
+        expect(makeGetUserBalanceController()).toBeInstanceOf(
+            GetUserBalanceController,
+        )
     })
 })
