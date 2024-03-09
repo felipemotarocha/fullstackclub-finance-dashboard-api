d79063210c484c56ae5e4cfeabf80720637a5fb4
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-08 23:29:41 -0300

    test: ensure PostgresUpdateTransactionRepository updates a transaction on db

---
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker'
+import dayjs from 'dayjs'
+import { TransactionType } from '@prisma/client'
+import { prisma } from '../../../../prisma/prisma'
+import { transaction, user } from '../../../tests'
+import { PostgresUpdateTransactionRepository } from './update-transaction'
+
+describe('PostgresUpdateTransactionRepository', () => {
+    it('should update a transaction on db', async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        })
+        const sut = new PostgresUpdateTransactionRepository()
+        const params = {
+            id: faker.string.uuid(),
+            user_id: user.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EXPENSE,
+            amount: Number(faker.finance.amount()),
+        }
+
+        const result = await sut.execute(transaction.id, params)
+
+        expect(result.name).toBe(params.name)
+        expect(result.type).toBe(params.type)
+        expect(result.user_id).toBe(user.id)
+        expect(String(result.amount)).toBe(String(params.amount))
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(params.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(params.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(params.date).year())
+    })
+})
