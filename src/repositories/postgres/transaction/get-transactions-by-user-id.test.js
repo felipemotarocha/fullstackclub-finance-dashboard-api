2a267dceb85a5de04476e6e1ab5ebeeb3d9c1e82
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-08 23:24:30 -0300

    test: ensure PostgresGetTransactionsByUserIdRepository throws if Prisma throws

--- src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
@@ -41,4 +41,15 @@ describe('PostgresGetTransactionsByUserIdRepository', () => {
             },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+        jest.spyOn(prisma.transaction, 'findMany').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(user.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
