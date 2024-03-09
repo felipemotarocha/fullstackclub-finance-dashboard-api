9ff4905b612fdba90d86ab964215e9817fd04e51
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-08 23:57:28 -0300

    feat: ensure transaction id is passed to TransactionNotFoundError

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -14,7 +14,7 @@ export class PostgresDeleteTransactionRepository {
             if (error instanceof PrismaClientKnownRequestError) {
                 // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
                 if (error.code === 'P2025') {
-                    throw new TransactionNotFoundError()
+                    throw new TransactionNotFoundError(transactionId)
                 }
             }
 
