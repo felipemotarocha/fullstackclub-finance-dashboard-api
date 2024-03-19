bba09319855e2f1fa002f3aec1fdc0261584842b
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 11:39:02 -0300

    test: ensure UpdateTransactionRepository throws TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transaction/update-transaction.test.js
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -4,6 +4,8 @@ import { TransactionType } from '@prisma/client'
 import { prisma } from '../../../../prisma/prisma'
 import { transaction, user } from '../../../tests'
 import { PostgresUpdateTransactionRepository } from './update-transaction'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
+import { TransactionNotFoundError } from '../../../errors'
 
 describe('PostgresUpdateTransactionRepository', () => {
     it('should update a transaction on db', async () => {
@@ -61,5 +63,20 @@ describe('PostgresUpdateTransactionRepository', () => {
         const promise = sut.execute(transaction.id, transaction)
 
         await expect(promise).rejects.toThrow()
+    })
+
+    it('should throw TransactionNotFoundError if Prisma does not find record to update', async () => {
+        const sut = new PostgresUpdateTransactionRepository()
+        jest.spyOn(prisma.transaction, 'update').mockRejectedValueOnce(
+            new PrismaClientKnownRequestError('', {
+                code: 'P2025',
+            }),
+        )
+
+        const promise = sut.execute(transaction.id, transaction)
+
+        await expect(promise).rejects.toThrow(
+            new TransactionNotFoundError(transaction.id),
+        )
     })
 })
