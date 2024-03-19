bc9563a4c6491270ee63eec7968705a62fc99a49
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 11:11:54 -0300

    refactor: update PostgresDeleteUserRepository test message

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -40,7 +40,7 @@ describe('PostgresDeleteUserRepository', () => {
         await expect(promise).rejects.toThrow()
     })
 
-    it('should throw generic error if Prisma throws generic error', async () => {
+    it('should throw UserNotFoundError if Prisma does not find record to delete', async () => {
         const sut = new PostgresDeleteUserRepository()
         jest.spyOn(prisma.user, 'delete').mockRejectedValueOnce(
             new PrismaClientKnownRequestError('', {
