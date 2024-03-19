4bc4086be057d1639ea4206110a7b401fb243d29
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 10:58:28 -0300

    test: ensure GET /api/users/:userId returns 404 when user is not found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -116,4 +116,12 @@ describe('User Routes E2E Tests', () => {
             balance: '6000',
         })
     })
+
+    it('GET /api/users/:userId should return 404 when user is not found', async () => {
+        const response = await request(app).get(
+            `/api/users/${faker.string.uuid()}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
 })
