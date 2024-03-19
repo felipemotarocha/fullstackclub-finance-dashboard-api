bb5deb9ea7cd4025e2c6a5c4fddba8334cd7cf06
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 11:14:26 -0300

    test: ensure POST /api/users returns 400 if email is already in use

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -145,4 +145,23 @@ describe('User Routes E2E Tests', () => {
 
         expect(response.status).toBe(404)
     })
+
+    it('POST /api/users should return 400 when the provided e-mail is already in use', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const response = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+                email: createdUser.email,
+            })
+
+        expect(response.status).toBe(400)
+    })
 })
