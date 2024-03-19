8105bd0d1dce54ff714d340a2437bc772dcdb9d5
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 10:06:37 -0300

    refactor: move user routes to separate file

--- index.js
+++ index.js
@@ -1,62 +1,19 @@
 import 'dotenv/config.js'
 import express from 'express'
-import {
-    makeCreateUserController,
-    makeDeleteUserController,
-    makeGetUserBalanceController,
-    makeGetUserByIdController,
-    makeUpdateUserController,
-} from './src/factories/controllers/user.js'
+
 import {
     makeCreateTransactionController,
     makeDeleteTransactionController,
     makeGetTransactionsByUserIdController,
     makeUpdateTransactionController,
 } from './src/factories/controllers/transaction.js'
+import { usersRouter } from './src/routes/users.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = makeGetUserByIdController()
-
-    const { statusCode, body } = await getUserByIdController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.get('/api/users/:userId/balance', async (request, response) => {
-    const getUserBalanceController = makeGetUserBalanceController()
-
-    const { statusCode, body } = await getUserBalanceController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.post('/api/users', async (request, response) => {
-    const createUserController = makeCreateUserController()
-
-    const { statusCode, body } = await createUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = makeUpdateUserController()
-
-    const { statusCode, body } = await updateUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.delete('/api/users/:userId', async (request, response) => {
-    const deleteUserController = makeDeleteUserController()
-
-    const { statusCode, body } = await deleteUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
+app.use('/api/users', usersRouter)
 
 app.get('/api/transactions', async (request, response) => {
     const getTransactionsByUserIdController =
@@ -71,9 +28,8 @@ app.get('/api/transactions', async (request, response) => {
 app.post('/api/transactions', async (request, response) => {
     const createTransactionController = makeCreateTransactionController()
 
-    const { statusCode, body } = await createTransactionController.execute(
-        request,
-    )
+    const { statusCode, body } =
+        await createTransactionController.execute(request)
 
     response.status(statusCode).send(body)
 })
@@ -81,9 +37,8 @@ app.post('/api/transactions', async (request, response) => {
 app.patch('/api/transactions/:transactionId', async (request, response) => {
     const updateTransactionController = makeUpdateTransactionController()
 
-    const { statusCode, body } = await updateTransactionController.execute(
-        request,
-    )
+    const { statusCode, body } =
+        await updateTransactionController.execute(request)
 
     response.status(statusCode).send(body)
 })
@@ -91,9 +46,8 @@ app.patch('/api/transactions/:transactionId', async (request, response) => {
 app.delete('/api/transactions/:transactionId', async (request, response) => {
     const deleteTransactionController = makeDeleteTransactionController()
 
-    const { statusCode, body } = await deleteTransactionController.execute(
-        request,
-    )
+    const { statusCode, body } =
+        await deleteTransactionController.execute(request)
 
     response.status(statusCode).send(body)
 })
---
+++ src/routes/users.js
@@ -0,0 +1,50 @@
+import { Router } from 'express'
+import {
+    makeCreateUserController,
+    makeDeleteUserController,
+    makeGetUserBalanceController,
+    makeGetUserByIdController,
+    makeUpdateUserController,
+} from '../factories/controllers/user.js'
+
+export const usersRouter = Router()
+
+usersRouter.get('/:userId', async (request, response) => {
+    const getUserByIdController = makeGetUserByIdController()
+
+    const { statusCode, body } = await getUserByIdController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+usersRouter.get('/:userId/balance', async (request, response) => {
+    const getUserBalanceController = makeGetUserBalanceController()
+
+    const { statusCode, body } = await getUserBalanceController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const createUserController = makeCreateUserController()
+
+    const { statusCode, body } = await createUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+usersRouter.patch('/:userId', async (request, response) => {
+    const updateUserController = makeUpdateUserController()
+
+    const { statusCode, body } = await updateUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+usersRouter.delete('/:userId', async (request, response) => {
+    const deleteUserController = makeDeleteUserController()
+
+    const { statusCode, body } = await deleteUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
