0347d857b15d794860647f340272c936283995b2
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 11:34:33 -0300

    test: ensure PATCH /api/transactions/:transactionId returns 200 when updating a transaction

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '../app.js'
 import { transaction, user } from '../tests/index.js'
+import { TransactionType } from '@prisma/client'
 
 describe('Transaction Routes E2E Tests', () => {
     it('POST /api/transactions should return 201 when creating a transaction successfully', async () => {
@@ -39,5 +40,26 @@ describe('Transaction Routes E2E Tests', () => {
 
         expect(response.status).toBe(200)
         expect(response.body[0].id).toBe(createdTransaction.id)
+    })
+
+    it('PATCH /api/transactions/:transactionId should return 200 when updating a transaction successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({ ...transaction, user_id: createdUser.id, id: undefined })
+
+        const response = await request(app)
+            .patch(`/api/transactions/${createdTransaction.id}`)
+            .send({ amount: 100, type: TransactionType.INVESTMENT })
+
+        expect(response.status).toBe(200)
+        expect(response.body.amount).toBe('100')
+        expect(response.body.type).toBe(TransactionType.INVESTMENT)
     })
 })
