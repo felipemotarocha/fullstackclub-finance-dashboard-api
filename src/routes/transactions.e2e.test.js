3e60f957cb90392c36557c0f88824bbe825f4e92
Felipe Rocha <felipemota.rocha@gmail.com>
2024-03-19 11:33:01 -0300

    test: ensure GET /api/transaction?userId returns 200 when fetching transactions successfully

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -20,4 +20,24 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.body.type).toBe(transaction.type)
         expect(response.body.amount).toBe(String(transaction.amount))
     })
+
+    it('GET /api/transaction?userId should return 200 when fetching transactions successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({ ...transaction, user_id: createdUser.id, id: undefined })
+
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toBe(createdTransaction.id)
+    })
 })
