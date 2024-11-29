32b7ffc803e3c2ce6f213b0228953830ab9b10fb
Felipe Rocha <felipemota.rocha@gmail.com>
2024-11-29 17:52:47 -0300

    test: ensure only authenticated user data can be fetched

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -16,7 +16,7 @@ describe('User Routes E2E Tests', () => {
         expect(response.status).toBe(201)
     })
 
-    it('GET /api/users/:userId should return 200 when user is found', async () => {
+    it('GET /api/users should return 200 when user is found', async () => {
         const { body: createdUser } = await request(app)
             .post('/api/users')
             .send({
@@ -25,7 +25,7 @@ describe('User Routes E2E Tests', () => {
             })
 
         const response = await request(app)
-            .get(`/api/users/${createdUser.id}`)
+            .get(`/api/users`)
             .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
 
         expect(response.status).toBe(200)
@@ -48,7 +48,7 @@ describe('User Routes E2E Tests', () => {
         }
 
         const response = await request(app)
-            .patch(`/api/users/${createdUser.id}`)
+            .patch(`/api/users`)
             .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
             .send(updateUserParams)
 
@@ -68,7 +68,7 @@ describe('User Routes E2E Tests', () => {
             })
 
         const response = await request(app)
-            .delete(`/api/users/${createdUser.id}`)
+            .delete(`/api/users`)
             .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
 
         expect(response.status).toBe(200)
@@ -108,7 +108,7 @@ describe('User Routes E2E Tests', () => {
         })
 
         const response = await request(app)
-            .get(`/api/users/${createdUser.id}/balance`)
+            .get(`/api/users/balance`)
             .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
 
         expect(response.status).toBe(200)
