b7415e77a1ed51d8e22684a834e7bcca79f57a2c
Felipe Rocha <felipemota.rocha@gmail.com>
2025-01-04 16:36:10 -0300

    feat: ensure from and to params are passed correctly to use case

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -26,7 +26,11 @@ export class GetTransactionsByUserIdController {
             })
 
             const transactions =
-                await this.getTransactionsByUserIdUseCase.execute(userId)
+                await this.getTransactionsByUserIdUseCase.execute(
+                    userId,
+                    from,
+                    to,
+                )
 
             return ok(transactions)
         } catch (error) {
--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,10 +1,14 @@
 import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
-    async execute(userId) {
+    async execute(userId, from, to) {
         return await prisma.transaction.findMany({
             where: {
                 user_id: userId,
+                date: {
+                    gte: new Date(from),
+                    lte: new Date(to),
+                },
             },
         })
     }
