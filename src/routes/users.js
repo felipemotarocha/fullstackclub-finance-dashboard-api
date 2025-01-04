fd395c5032c45f5056ae4dedcf7282cfdc79a24b
Felipe Rocha <felipemota.rocha@gmail.com>
2025-01-04 16:41:26 -0300

    feat: ensure /balance route receives from and to query params and pass it to controler

--- src/routes/users.js
+++ src/routes/users.js
@@ -35,6 +35,10 @@ usersRouter.get('/balance', auth, async (request, response) => {
         params: {
             userId: request.userId,
         },
+        query: {
+            from: request.query.from,
+            to: request.query.to,
+        },
     })
 
     response.status(statusCode).send(body)
